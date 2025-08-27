7b19f0b0120cab5620be1622c9e0afaf06583679
catherine-sarquis <catherinesarquis@gmail.com>
2025-08-28 00:13:46 +0100

    add form to dictionary component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Footer from "./Footer";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <div className="flex flex-col w-5/6 p-8 m-auto">
         <main>
           <h1 className="text-xl text-center md:text-5xl">Definitive</h1>
+          <Dictionary />
         </main>
         <Footer />
       </div>
---
+++ src/Dictionary.jsx
@@ -0,0 +1,13 @@
+export default function Dictionary() {
+  return (
+    <form action="search">
+      <input
+        type="search"
+        name="search"
+        id="search"
+        autoFocus
+        className="border"
+      />
+    </form>
+  );
+}
