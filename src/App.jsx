2b25feb7d4a22fafbb7efd5975f42d1da76f35d5
catherine-sarquis <catherinesarquis@gmail.com>
2025-08-27 23:46:16 +0100

    set up app container with main and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Footer from "./Footer";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-col w-5/6 p-8 m-auto">
+        <main>
+          <h1 className="text-xl text-center md:text-5xl">Definitive</h1>
+        </main>
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code className="size-48">src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="text-sky-500">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
