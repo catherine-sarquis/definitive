3e46014c1c7dfb37a9fda425e91d84006e532c7c
catherine-sarquis <catherinesarquis@gmail.com>
2025-08-28 09:59:41 +0100

    apply state to search keyword

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 export default function Dictionary() {
+  const [keyword, setkeyword] = useState(null);
+  function search(event) {
+    event.preventDefault();
+    alert("searching");
+  }
+
+  function handleKeywordChange(event) {
+    setkeyword(event.target.value);
+  }
+
   return (
-    <form action="search">
+    <form onSubmit={search}>
       <input
         type="search"
         name="search"
         id="search"
         autoFocus
         className="border"
+        onChange={handleKeywordChange}
       />
+      <button type="submit">Search</button>
     </form>
   );
 }
