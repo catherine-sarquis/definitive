ec0948f581e1ce425581edbf507667604504c90b
catherine-sarquis <catherinesarquis@gmail.com>
2025-08-28 21:20:22 +0100

    adds link to images

--- src/Images.jsx
+++ src/Images.jsx
@@ -5,12 +5,14 @@ export default function Images(props) {
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mb-6 md:mb-8">
         {props.images.photos.map(function (image, index) {
           return (
-            <img
-              src={image.src.landscape}
-              alt={image.alt}
-              key={index}
-              className="object-cover rounded-sm outline outline-indigo-600 shadow-md"
-            />
+            <a href={image.src.original} target="_blank" rel="noreferrer">
+              <img
+                src={image.src.landscape}
+                alt={image.alt}
+                key={index}
+                className="object-cover rounded-sm outline outline-indigo-600 shadow-md"
+              />
+            </a>
           );
         })}
       </div>
