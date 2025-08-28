019fe0427bdcdb0ca974d36e9d90ea9ecfd1a154
catherine-sarquis <catherinesarquis@gmail.com>
2025-08-28 21:33:57 +0100

    adds footer text

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,24 @@
 export default function Footer() {
   return (
     <>
-      <p>this is the footer</p>
+      <p className="text-center text-xs">
+        A little project by{" "}
+        <a
+          href="https://github.com/catherine-sarquis"
+          className="text-indigo-600"
+        >
+          Catherine.
+        </a>
+        <br></br>
+        View the code on{" "}
+        <a
+          href="https://github.com/catherine-sarquis/definitive"
+          className="text-indigo-600"
+        >
+          Github
+        </a>
+        .
+      </p>
     </>
   );
 }
