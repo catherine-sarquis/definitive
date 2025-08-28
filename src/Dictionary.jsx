1cc885a76ea1def4c50574711d18e6e3e009f83c
catherine-sarquis <catherinesarquis@gmail.com>
2025-08-28 10:32:15 +0100

    fetch data with axios

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
+import axios from "axios";
+
 export default function Dictionary() {
   const [keyword, setkeyword] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert("searching");
+    const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=c3ae07f646b904bce9d83oat69c5764d`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
